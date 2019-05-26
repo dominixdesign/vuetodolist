a5f5679116a7c88d2a042a0ddd9ed320223a5893
Dominik Sander <dominik.sander@googlemail.com>
2019-05-26 22:26:41 +0200

    remove faker and use static data

--- src/store.js
+++ src/store.js
@@ -1,27 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import faker from 'faker'
+import todos from './example.data'
 
 Vue.use(Vuex)
 
-console.log()
-
-// Helper function to generate example todos
-const generateTodoList = numberOfTodo  => {
-  var todoArray = []
-  for(;numberOfTodo--;numberOfTodo>0) {
-    todoArray.push({
-      title: faker.fake('{{lorem.words}}'),
-      desc: faker.fake('{{lorem.paragraphs}}'),
-      status: Math.round(Math.random())
-    })
-  }
-  return todoArray
-}
-
 export default new Vuex.Store({
   state: {
-    todos: generateTodoList(10)
+    todos
   },
   mutations: {
     triggerStatus(state, payload) {
